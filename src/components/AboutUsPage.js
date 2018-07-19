86a15f31017dbd825275347607ae2151e0b1f80c
Jose Valdvogel <valdvogel@hotmail.com>
2018-07-19 15:10:31 -0700

    add text on aboutus page

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 const AboutUsPage = () => (
     <div>
-        
-
+        <section id="main" className="cadastro-wrapper">
+            <div className="inner">
+                <header className="align-center">
+                    <h2>Quem somos</h2>
+                </header>
+                <p align="justify">
+                    A eVolume tem como um de seus propósitos, conectar viajantes, aventureiros, esportistas, famílias, dentre outros; com proprietários de equipamentos para transporte de grandes volumes com segurança, tais como, bagageiros, racks, bebês-conforto e cadeirinhas, kits de segurança para pets e malas para bikes; tudo isso de forma única e segura através de nossa plataforma digital.
+                    <br/>
+                    Inicialmente, buscávamos atender uma necessidade pessoal, quando vislumbramos a gravidade da questão de segurança existente neste contexto. Desde então não medimos esforços para buscar transformar a ideia em algo com um propósito maior.
+                    <br/>
+                    Servare vitas, do latim, salvar vidas! Ao imaginarmos ações correlatas à frase anterior, imaginamos a mais nobre das profissões, os bombeiros. Contudo, acreditamos que, salvar vidas, pode ser resultado de inúmeras outras ações que não apenas resgates realizados pelos nobres bombeiros. Criar novas oportunidades, através da aplicação do conceito de economia compartilhada e colaborativa, pode resultar no “salvamento de vidas”, através da transformação e geração de novas oportunidades.
+                    <br/>
+                    Disseminar a ideia e o uso de equipamentos que promovem maior segurança no transporte automotivo, para a sociedade como um todo e ainda permitir a criação de novos negócios baseados no conceito de economia criativa, promovendo a transformação de forma compartilhada.
+                    <br/>
+                    Nós nos sentimos realizados com o grande propósito de aumentar a segurança e salvar vidas quanto ao transporte de famílias, esportistas e aventureiros; bem como, na transformação de vidas.
+                </p>
+            </div>
+        </section>
     </div>
 
-);
-
+        );
+        
 export default AboutUsPage;
