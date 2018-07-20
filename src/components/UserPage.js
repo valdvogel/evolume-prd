6f667f85542cdcc5a004f036ba8dd035ad02f7c4
Jose Valdvogel <valdvogel@hotmail.com>
2018-07-19 17:49:39 -0700

    add google and facebook button for new users

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,63 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
 import UserForm from './UserForm';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
-
-export default class UserFormPage extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      modalIsOpen: true
-    };
-
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-
-  openModal() {
-    this.setState({ modalIsOpen: true });
-  }
-
-  afterOpenModal() {
-    this.subtitle.style.color = 'FFFFFF';
-  }
-
-  closeModal() {
-    this.setState({ modalIsOpen: false });
-  }
-
-  render() {
-    return (
-      <div>
-        <section id="main" className="cadastro-wrapper">
+export const UserFormPage = ({ startLoginGoogle, startLoginFacebook }) => (
+  <div>
+      <section id="main" className="cadastro-wrapper">
           <div className="inner">
-            <header className="align-center">
-              <h2>Cadastro</h2>
-            </header>
+              <header className="align-center">
+                  <h2>Cadastro</h2>
+              </header>
               <div className="w3l-main">
-                <div className="w3l-from">
-                  <UserForm />
-                </div>
+                  <div className="w3l-from">
+                      <button onClick={startLoginGoogle} className="loginBtn loginBtn--google">Entrar com Google</button>
+                      <button onClick={startLoginFacebook} className="loginBtn loginBtn--facebook">Entrar com Facebook</button>
+                      <br/>
+                      <br/>
+                      <UserForm />
+                  </div>
               </div>
-            </div>
-        </section>
-      </div>
-    );
-  }
-}
+          </div>
+      </section>
+  </div>
+);
 
+const mapDispatchToProps = (dispatch) => ({
+  startLoginGoogle: () => dispatch(startLogin('Google')),
+  startLoginFacebook: () => dispatch(startLogin('Facebook'))
 
+});
 
+export default connect(undefined, mapDispatchToProps)(UserFormPage);
